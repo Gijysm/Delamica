e931921865f0d60961d75405688e62b3f1917eaf
popkagnoma23@hotmail.com <popkagnoma23@hotmail.com>
2024-08-22 19:56:05 +0300

    #Language reterns

---
+++ src/Tokenization.h
@@ -0,0 +1,102 @@
+﻿#ifndef TOKENIZATION_H
+#define TOKENIZATION_H
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
+enum class Token_Type {
+  _back_roll,
+  _integer,
+  semi
+};
+struct Token {
+  Token_Type type;
+  optional<string> value {};
+};
+class Tokenizer
+    {
+  private:
+  const string msr_c;
+  int m_index;
+  [[nodiscard]] optional<char> peak(int ahead = 1) const
+  {
+    if (m_index + 1 >msr_c.length()) {
+      return{};
+    }
+    else {
+      return msr_c.at(m_index);
+    }
+  }
+  char consume() {
+    return msr_c.at(m_index++);
+  }
+  public:
+    inline Tokenizer(string str): msr_c(move(str)) {
+
+    }
+    inline vector<Token> tokenize(const string& str) {
+      string buf;
+      vector<Token> tokens;
+
+      while(peak().has_value()) {
+        if(isalpha(peak().value())) {
+          buf += consume();
+        }
+        while(peak().has_value()) {
+          buf += consume();
+        }
+      }
+      for (int i = 0; i < str.length(); i++) {
+        char a = str.at(i);
+        if(isalpha(a)) {
+          buf += a;
+          i++;
+          while(isalnum(str.at(i))) {
+            buf.push_back(str.at(i));
+            i++;
+          }
+          i--;
+          if(buf == "backroll") {
+            tokens.push_back({.type = Token_Type::_back_roll});
+            buf.clear();
+            continue;
+          }
+          else {
+            cerr <<"Token type error" << ": 1" << a <<endl;
+            exit(EXIT_FAILURE);
+          }
+        }
+        else if (isdigit(a)) {
+
+
+          while (isdigit(str.at(i))) {
+            buf.push_back(str.at(i));
+            i++;
+          }
+          i--;
+          tokens.push_back({.type = Token_Type::_integer, .value = buf});
+          buf.clear();
+        }
+
+        else if(a == ';') {
+          tokens.push_back({.type = Token_Type::semi});
+        }
+        else if(isspace(a)) {
+          continue;
+        }
+        else {
+          cerr << "Token type error: 2, unexpected character: " << a << endl;
+          exit(EXIT_FAILURE);
+        }
+      }
+      return tokens;
+    }
+    ~Tokenizer();
+
+  };
+#endif //TOKENIZATION_H
---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+﻿#include "Tokenization.h"
+using namespace std;
+vector<Token> tokenize(const string& str) {
+
+}
+string token_to_asm(const vector<Token>& tokens) {
+    stringstream output;
+    output << "global _start\n_start:\n";
+    for (int i = 0; i < tokens.size(); i++) {
+        const Token& token = tokens[i];
+        if(token.type == Token_Type::_back_roll) {
+            if(i + 1 < tokens.size() && tokens[i + 1].type == Token_Type::_integer) {
+                if(i + 2 < tokens.size() && tokens[i + 2].type == Token_Type::semi) {
+                    output<< "      mov rax, 60\n";
+                    output<< "      mov rdi, " << tokens.at(i + 1).value.value() << "\n";
+                    output<< "      syscall";
+                }
+            }
+        }
+
+    }
+    return output.str();
+}
+int main(int argc, char* argv[])
+{
+    if (argc != 2)
+    {
+        cout << "Usage: " << argv[0] << " <filename>" << endl;
+        return EXIT_FAILURE;
+    }
+    string res;
+    {
+    stringstream ss;
+    fstream file(argv[1], ios::in);
+    ss << file.rdbuf();
+    file.close();
+        res = ss.str();
+    }
+
+    vector<Token> token = tokenize(res);
+    {
+        fstream out_file("out.asm", ios::out);
+        out_file << token_to_asm(token);
+    }
+    system("nasm -felf64 out.asm");
+    system("ld -o out out.o");
+    return EXIT_FAILURE;
+
+}
+
